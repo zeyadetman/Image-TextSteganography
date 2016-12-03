9469328838c415506de2ebb618b86da9a6306a25
Zeyad Etman <zeyadetman@yahoo.com>
2016-12-03 06:22:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Image_Viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(17, 45);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(144, 23);
+            this.OpenFile.TabIndex = 0;
+            this.OpenFile.Text = "Source File Location";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Destination File Location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(309, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 262);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Image_Viewer
+{
+
+    public partial class Form1 : Form
+    {
+        //public static string GetExtension(string path);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string imagetype,imagepath;                //store the type of file to make the operation on it
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "BMP File|*.bmp|TEXT File|*.txt";
+            
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK) // determine the type of file
+            {
+                imagepath = ofd.FileName.ToString();              //show the path on textbox
+                textBox1.Text = imagepath;           
+                if (Path.GetExtension(ofd.FileName) == ".bmp")   // if file -> bmp ,then imagetype -> bmp ,else imagetype ->text 
+                {
+                    imagetype = "bmp";
+                }
+                else
+                {
+                    imagetype = "text";
+                }
+                
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        string file2path;               // file2 path based on type of file1
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dest = new OpenFileDialog();
+            if (imagetype == "bmp")
+            {
+                dest.Filter = "TEXT File|*.txt";
+                if (dest.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {    
+                }
+            }
+            else
+            {
+                dest.Filter = "BMP File|*.bmp";
+                if (dest.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {   
+                }
+            }
+
+            file2path = dest.FileName.ToString();              //show the path of destination file on textbox2
+            textBox2.Text = file2path;      
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Bitmap bmpimage;
+        int iheight = 0, iwidth = 0;   //to store height and width of the image
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(file2path);
+            if (imagetype == "bmp")
+            {
+                bmpimage = new Bitmap(imagepath, true);
+                iwidth = bmpimage.Width; iheight = bmpimage.Height;
+                
+                file.WriteLine(iwidth);
+                file.WriteLine(iheight);
+                for (int i = 0; i < iwidth; i++)
+                {
+                    for (int j = 0; j < iheight; j++)
+                    {
+                        Color pixelColor = bmpimage.GetPixel(i, j);
+                        Color newColor = Color.FromArgb(pixelColor.R, pixelColor.G, pixelColor.B);
+                        file.WriteLine(pixelColor.R);
+                        file.WriteLine(pixelColor.G);
+                        file.WriteLine(pixelColor.B);
+                    }
+                }
+                
+            }
+            else
+            {
+            }
+            file.Close();
+        }
+    }
+}
