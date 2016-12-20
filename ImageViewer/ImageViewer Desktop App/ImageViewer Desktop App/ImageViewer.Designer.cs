9ecc2217781a30d4f7ad74b5eca4e48f009f0aec
Zeyad Etman <zeyadetman@gmail.com>
2016-12-20 02:44:40 -0800

    Image Viewer Desktop App

---
+++ ImageViewer/ImageViewer Desktop App/ImageViewer Desktop App/ImageViewer.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ImageViewer_Desktop_App
+{
+    partial class ImageViewerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textToolStripMenuItem1 = new System.Windows.Forms.ToolStripTextBox();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.encryptionToolStripMenuItem,
+            this.decryptionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(591, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageToolStripMenuItem,
+            this.textToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.imageToolStripMenuItem.Text = "Image";
+            this.imageToolStripMenuItem.Click += new System.EventHandler(this.imageToolStripMenuItem_Click);
+            // 
+            // textToolStripMenuItem
+            // 
+            this.textToolStripMenuItem.Name = "textToolStripMenuItem";
+            this.textToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.textToolStripMenuItem.Text = "Text";
+            this.textToolStripMenuItem.Click += new System.EventHandler(this.textToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asImageToolStripMenuItem,
+            this.asTextToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // asImageToolStripMenuItem
+            // 
+            this.asImageToolStripMenuItem.Name = "asImageToolStripMenuItem";
+            this.asImageToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.asImageToolStripMenuItem.Text = "As Image";
+            this.asImageToolStripMenuItem.Click += new System.EventHandler(this.asImageToolStripMenuItem_Click);
+            // 
+            // asTextToolStripMenuItem
+            // 
+            this.asTextToolStripMenuItem.Name = "asTextToolStripMenuItem";
+            this.asTextToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.asTextToolStripMenuItem.Text = "As Text";
+            this.asTextToolStripMenuItem.Click += new System.EventHandler(this.asTextToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // encryptionToolStripMenuItem
+            // 
+            this.encryptionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.encryptToolStripMenuItem});
+            this.encryptionToolStripMenuItem.Name = "encryptionToolStripMenuItem";
+            this.encryptionToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.encryptionToolStripMenuItem.Text = "Encryption";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textToolStripMenuItem1,
+            this.fileToolStripMenuItem1});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // textToolStripMenuItem1
+            // 
+            this.textToolStripMenuItem1.MaxLength = 1000;
+            this.textToolStripMenuItem1.Name = "textToolStripMenuItem1";
+            this.textToolStripMenuItem1.Size = new System.Drawing.Size(152, 23);
+            this.textToolStripMenuItem1.Text = "Text";
+            this.textToolStripMenuItem1.DoubleClick += new System.EventHandler(this.textToolStripMenuItem1_DoubleClick);
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(212, 22);
+            this.fileToolStripMenuItem1.Text = "File";
+            this.fileToolStripMenuItem1.Click += new System.EventHandler(this.fileToolStripMenuItem1_Click);
+            // 
+            // encryptToolStripMenuItem
+            // 
+            this.encryptToolStripMenuItem.Name = "encryptToolStripMenuItem";
+            this.encryptToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.encryptToolStripMenuItem.Text = "Encrypt";
+            this.encryptToolStripMenuItem.Click += new System.EventHandler(this.encryptToolStripMenuItem_Click);
+            // 
+            // decryptionToolStripMenuItem
+            // 
+            this.decryptionToolStripMenuItem.Name = "decryptionToolStripMenuItem";
+            this.decryptionToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.decryptionToolStripMenuItem.Text = "Decryption";
+            this.decryptionToolStripMenuItem.Click += new System.EventHandler(this.decryptionToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 348);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ImageViewerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 400);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ImageViewerApp";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem encryptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem encryptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decryptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox textToolStripMenuItem1;
+    }
+}
+
---
+++ ImageViewer/ImageViewer Desktop App/ImageViewer Desktop App/ImageViewer.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing.Graphics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ImageViewer_Desktop_App
+{
+    public partial class ImageViewerApp : Form
+    {
+        public ImageViewerApp()
+        {
+            InitializeComponent();
+        }
+
+        ///Variables
+        public string usrimgpath;    //store the user image path
+        public string usrtextpath;   //store the user text path
+        public string savtxtfile;    //store the location of saved text file
+        public string savimgfile;    //store the location of saved image file
+        public Bitmap imageco;   //non-important variable - used outside to be public 
+        public Bitmap loadimg;   //non-important variable - used outside to be public
+        public string encryptedtext;        //before encryption
+        public string encryptedfile;        // path of text to encrypt
+        public string encryptedfiletext;    //store words 
+        public string encryptedtextbox;    //store words from txt
+
+
+
+        /// <summary>
+        /// covert from text to image
+        /// </summary>
+        /// <returns>Bitmap image</returns>
+        public Bitmap converttoimage(Bitmap img,string urtxtpath)
+        {
+            using (StreamReader file = new StreamReader(urtxtpath))
+            {
+                int wid = int.Parse(file.ReadLine());
+                int hei = int.Parse(file.ReadLine());
+                img = new Bitmap(wid, hei);
+                int r, g, b;
+                for (int i = 0; i < hei; i++)
+                {
+                    for (int j = 0; j < wid; j++)
+                    {
+                        r = int.Parse(file.ReadLine());
+                        g = int.Parse(file.ReadLine());
+                        b = int.Parse(file.ReadLine());
+                        img.SetPixel(j, i, Color.FromArgb(r, g, b));
+
+                    }
+                }
+
+            }
+            return img;
+        }
+
+
+        /// <summary>
+        /// Open The image and display it on App
+        /// </summary>
+        private void imageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog usrimg = new OpenFileDialog();
+            usrimg.Filter = "Image | *.jpg; *.jpeg; *.jpe; *.jfif; *.bmp; *.png";
+            if (usrimg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                usrimgpath = usrimg.FileName.ToString();              //Open the user image
+            }
+            if (usrimgpath != null)            //check if the image opened or not to display it
+            {
+                pictureBox1.Image = Image.FromFile(usrimgpath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+        
+        /// <summary>
+        /// Open The text, and open the image on App
+        /// </summary>
+        private void textToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog usrtext = new OpenFileDialog();
+            usrtext.Filter = "Text | *.txt";
+            if (usrtext.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                usrtextpath = usrtext.FileName.ToString();              //Load the user text file
+            }
+            if (usrtextpath != null)
+            {
+                Bitmap im;
+                im = converttoimage(imageco, usrtextpath);
+                pictureBox1.Image = im;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+
+        /// <summary>
+        /// determine saved text location
+        /// </summary>
+        private void asTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (usrimgpath != null)
+            {
+            SaveFileDialog saveastext = new SaveFileDialog();
+            saveastext.Filter = "Text|*.txt";
+            saveastext.Title = "Save it as text";
+            saveastext.ShowDialog();
+            savtxtfile = saveastext.FileName.ToString();
+            if (saveastext.FileName != null)
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveastext.OpenFile();
+                fs.Close();   
+            }
+            if (savtxtfile != null)
+            {
+                using (StreamWriter file = new StreamWriter(savtxtfile, true))
+                {
+                    Bitmap loadimg;
+                    loadimg = new Bitmap(usrimgpath, true);
+                    file.WriteLine(loadimg.Width);
+                    file.WriteLine(loadimg.Height);
+                    for (int i = 0; i < loadimg.Height; i++)
+                    {
+                        for (int j = 0; j < loadimg.Width; j++)
+                        {
+                            Color pixelColor = loadimg.GetPixel(j, i);
+                            Color newColor = Color.FromArgb(pixelColor.R, pixelColor.G, pixelColor.B);
+                            file.WriteLine(pixelColor.R);
+                            file.WriteLine(pixelColor.G);
+                            file.WriteLine(pixelColor.B);
+                        }
+                    }
+                    file.Close();
+                }
+            }
+            
+                
+            }
+            else
+            {
+                MessageBox.Show("Please load an image first.");    
+            }
+
+        }
+
+
+        /// <summary>
+        /// determine saved image location
+        /// </summary>
+        private void asImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (usrtextpath != null)
+            {
+                SaveFileDialog saveasimg = new SaveFileDialog();
+                saveasimg.Filter = "Image |*.PNG";
+                saveasimg.Title = "Save it as image";
+                saveasimg.ShowDialog();
+                if (saveasimg.FileName != null)
+                {
+                    System.IO.FileStream fs = (System.IO.FileStream)saveasimg.OpenFile();
+                    fs.Close();
+                }
+                Bitmap reloadimg;
+                reloadimg = converttoimage(loadimg, usrtextpath);
+                pictureBox1.Image = reloadimg;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                savimgfile = saveasimg.FileName.ToString();
+                pictureBox1.Image.Save(savimgfile, System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+            else
+            {
+                SaveFileDialog saveasimg = new SaveFileDialog();
+                saveasimg.Filter = "Image |*.PNG";
+                saveasimg.Title = "Save it as image";
+                saveasimg.ShowDialog();
+                if (saveasimg.FileName != null)
+                {
+                    File.Copy(usrimgpath, saveasimg.FileName.ToString());
+                }
+                
+            }
+            
+
+        }
+
+         
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Assembly Part
+        /// </summary>
+        
+        [DllImport("Project.dll")]
+        private static extern IntPtr Encrypt([In, Out] char[] path, [In, Out] char[] text);
+        [DllImport("Project.dll")]
+        private static extern IntPtr Decrypt([In] char[] path);
+
+        private void Decryption(string path_str)
+        {
+            var path = path_str.ToCharArray();
+            var s = Decrypt(path);
+            MessageBox.Show(Marshal.PtrToStringAnsi(s));
+        }
+
+        private void Encryption(string path_str, string text)
+        {
+            var path = path_str.ToCharArray();
+            char[] msg = new char[125000];
+            text.ToCharArray().CopyTo(msg, 0);
+            Encrypt(path, msg);
+            MessageBox.Show("Done!");
+        }
+
+        private void fileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog encryptedf = new OpenFileDialog();
+            encryptedf.Filter = "Text | *.txt";
+            if (encryptedf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                encryptedfile = encryptedf.FileName.ToString();              //Load the text file to encrypt
+            }
+            if (encryptedfile != null)
+            {
+                    encryptedfiletext = System.IO.File.ReadAllText(encryptedfile);
+            }
+
+        }
+
+        private void textToolStripMenuItem1_DoubleClick(object sender, EventArgs e)
+        {
+            encryptedtextbox = textToolStripMenuItem1.Text.ToString();
+        }
+
+        private void textToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (encryptedfiletext != null)
+            {
+                Encryption(usrtextpath, encryptedfiletext);
+            }
+            
+        }
+
+        private void encryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (encryptedfiletext != null && usrtextpath != null)
+            {
+                Encryption(usrtextpath, encryptedfiletext);
+            }
+            else if (encryptedtextbox != null && usrtextpath != null)
+            {
+                Encryption(usrtextpath, encryptedtextbox);
+            }
+
+        }
+
+        private void decryptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           Decryption(usrtextpath);
+        }
+ 
+    }
+}
